48b0d88a8147d4c039a02c0f98ca96a29cb4c14d
hungd2z <tienhung14723@gmail.com>
2021-09-29 12:38:39 +0700

    First commit

---
+++ utils/log.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk');
+
+module.exports = (data, option) => {
+	switch (option) {
+		case "warn":
+			console.log(chalk.yellow('[ ❕ Lỗi rồi ] » ') + data);
+			break;
+		case "error":
+			console.log(chalk.red('[ ❕ Lỗi rồi ] » ') + data);
+			break;
+		default:
+			console.log(chalk.magenta(`${option} » `) + data);
+			break;
+	}
+}
+
+module.exports.loader = (data, option) => {
+	switch (option) {
+		case "warn":
+			console.log(chalk.yellow('[ MARCUS ] » ') + data);
+			break;
+		case "error":
+			console.log(chalk.red('[ MARCUS ] » ') + data);
+			break;
+		default:
+			console.log(chalk.green(`[ MARCUS ] » `) + data);
+			break;
+	}
+}
